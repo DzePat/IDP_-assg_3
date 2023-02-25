a59b1e572a840223c220390075adc1f489df582b
DzePat <ddzedas@gmail.com>
2023-02-25 10:58:36 +0100

    added an array of 6 indexes, assigned each index to a element

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@
         }
 
         //uppgift 2 add 3 elements syre,järn, guld
+        //uppgift 4 made an array of 6 elements, assigned 3 existing elements to 0-2 index and added 3 new elements to the remaining indexes
         public static void Main(string[] args)
         {
             Element syre = new Element() { namn = "syre", atomnummer = "8", typ = "ickemetall", smältpunkt = "55.36", kokpunkt = "90.188" };
@@ -27,6 +28,33 @@
             syre.print();
             järn.print();
             guld.print();
+            Element[] elements = new Element[6]
+                {
+                syre,
+                järn,
+                guld,
+                new Element()
+                {
+                    namn = "väte",
+                    atomnummer = "1",
+                    typ = "ickemetall",
+                    smältpunkt = "13.99",
+                    kokpunkt = "20.271"},
+                new Element()
+                {
+                    namn = "brom",
+                    atomnummer = "35",
+                    typ = "ickemetall",
+                    smältpunkt = "265.8",
+                    kokpunkt = "332.0"},
+                new Element()
+                {
+                    namn = "kvicksilver",
+                    atomnummer = "80",
+                    typ = "metall",
+                    smältpunkt = "234.3210",
+                    kokpunkt = "629.88"},
+                };
 
         }
     }
