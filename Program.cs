e83d7a9d74a6785d0ee6d4265854d36104dd69dc
DzePat <ddzedas@gmail.com>
2023-02-25 10:29:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Element
+{
+    class MainClass
+    {
+
+        class Element {
+
+            public string namn, atomnummer, typ, smältpunkt, kokpunkt;
+        
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+           
+             
+        }
+    }
+}
