bfad1a0985a3e893b18fb42bef053a7652344ce8
DzePat <ddzedas@gmail.com>
2023-02-25 11:18:14 +0100

    changed the type of existing strings of boiling point and melting point to double for easier calculations, added new foreach loop that checks if an element is above a certain boiling point and below a certain melting point.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@
         //uppgift 3 add print() to the class
         class Element {
 
-            public string namn, atomnummer, typ, smältpunkt, kokpunkt;
+            public string namn, atomnummer, typ;
+            public double smältpunkt, kokpunkt;
 
             public void print()
             {
@@ -21,9 +22,9 @@
         //uppgift 2 add 3 elements syre,järn, guld
         public static void Main(string[] args)
         {
-            Element syre = new Element() { namn = "syre", atomnummer = "8", typ = "ickemetall", smältpunkt = "55.36", kokpunkt = "90.188" };
-            Element järn = new Element() { namn = "järn", atomnummer = "26", typ = "metall", smältpunkt = "1811", kokpunkt = "3134" };
-            Element guld = new Element() { namn = "guld", atomnummer = "79", typ = "metall", smältpunkt = "1337.33", kokpunkt = "3243" };
+            Element syre = new Element() { namn = "syre", atomnummer = "8", typ = "ickemetall", smältpunkt = 55.36, kokpunkt = 90.188 };
+            Element järn = new Element() { namn = "järn", atomnummer = "26", typ = "metall", smältpunkt = 1811, kokpunkt = 3134 };
+            Element guld = new Element() { namn = "guld", atomnummer = "79", typ = "metall", smältpunkt = 1337.33, kokpunkt = 3243 };
             syre.print();
             järn.print();
             guld.print();
@@ -38,22 +39,22 @@
                     namn = "väte",
                     atomnummer = "1",
                     typ = "ickemetall",
-                    smältpunkt = "13.99",
-                    kokpunkt = "20.271"},
+                    smältpunkt = 13.99,
+                    kokpunkt = 20.271},
                 new Element()
                 {
                     namn = "brom",
                     atomnummer = "35",
                     typ = "ickemetall",
-                    smältpunkt = "265.8",
-                    kokpunkt = "332.0"},
+                    smältpunkt = 265.8,
+                    kokpunkt = 332.0},
                 new Element()
                 {
                     namn = "kvicksilver",
                     atomnummer = "80",
                     typ = "metall",
-                    smältpunkt = "234.3210",
-                    kokpunkt = "629.88"},
+                    smältpunkt = 234.3210,
+                    kokpunkt = 629.88},
                 };
             //uppgift 5 add a foreach loop that prints all the elements out from the array
             foreach(Element element in elements)
@@ -68,6 +69,14 @@
                     Console.WriteLine(element.namn);
                 }
             };
+            //uppgift 7 add a foreach loop that prints out the element that only has melting point under 273.16K and boiling point above 273.16K
+            foreach(Element element in elements)
+            {
+                if (element.smältpunkt < 273.16 && element.kokpunkt > 273.16)
+                {
+                    Console.WriteLine(element.namn);
+                }
+            }
         }
     }
 }
