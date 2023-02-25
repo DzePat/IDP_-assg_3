903bc06b2c89fe51e2f8da6382e5b0abab51b805
DzePat <ddzedas@gmail.com>
2023-02-25 10:37:30 +0100

    added three new elements

--- Program.cs
+++ Program.cs
@@ -12,9 +12,11 @@
 
         public static void Main(string[] args)
         {
+            Element syre = new Element() { namn = "syre", atomnummer = "8", typ = "ickemetall", smältpunkt = "55.36", kokpunkt = "90.188" };
+            Element järn = new Element() { namn = "järn", atomnummer = "26", typ = "metall", smältpunkt = "1811", kokpunkt = "3134" };
+            Element guld = new Element() { namn = "guld", atomnummer = "79", typ = "metall", smältpunkt = "1337.33", kokpunkt = "3243" };
 
-           
-             
+
         }
     }
 }
